5399a7cd9362e913e1619b824b26f59a4b8ba7e7
Sergey Shpadyrev <sergey@paytweed.com>
2022-12-08 12:07:46 +0200

    Add example

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { Environment, TweedBackendSDK } = require("@paytweed/backend-sdk");
+
+const authenticatedUser = {
+  email: "test@example.com",
+  id: "1",
+};
+
+const sdk = TweedBackendSDK.setup({
+  apiKey: "YOUR_API_KEY",
+  apiSecret: "YOUR_API_SECRET",
+  environment: Environment.production,
+});
+
+const app = express();
+app.use(express.json());
+
+app.post("/message", async (req, res) => {
+  const answer = await sdk.handleMessageFromFrontend(
+    req.body.message,
+    authenticatedUser.id,
+    authenticatedUser.email
+  );
+  res.send({ answer });
+});
+
+const port = 3001;
+app.listen(port, () => console.log(`App is listening on port ${port}`));
