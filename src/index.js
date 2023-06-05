6a340fbbf61add5595c20284fef9e4a42ae9ab17
Yoav Assulin <yoav@paytweed.com>
2023-06-05 22:22:40 +0300

    feat: support nft checkout

--- src/index.js
+++ src/index.js
@@ -1,28 +1,41 @@
-const cors = require("cors");
-const express = require("express");
-const { Environment, TweedBackendSDK } = require("@paytweed/backend-sdk");
+const cors = require('cors');
+const express = require('express');
+const { Environment, TweedBackendSDK } = require('@paytweed/backend-sdk');
 
 const authenticatedUser = {
-  email: "test@example.com",
-  id: "1",
+  email: 'test@example.com',
+  id: '22',
 };
 
 const start = async () => {
   const sdk = await TweedBackendSDK.setup({
-    apiKey: "YOUR_API_KEY",
-    apiSecret: "YOUR_API_SECRET",
-    defaultBlockchainIds: ["tezosGhost", "polygonMumbai", "ethereumGoerli"],
+    apiKey: 'YOUR_API_KEY',
+    apiSecret: 'YOUR_API_SECRET',
+    defaultBlockchainIds: ['tezosGhost', 'polygonMumbai', 'ethereumGoerli'],
+    callbacks: {
+      getNftPurchaseData: async ({ nftId }) => ({
+        nftId,
+        priceInCents: 100,
+        fiatCurrencyId: 'USD',
+        tokenUri: 'NFT_TOKEN_URI',
+        contractAddress: 'CONTRACT_ADDRESS',
+        chain: 'polygonMumbai',
+        title: 'NFT_TITLE',
+        description: 'NFT_DESCRIPTION',
+        abi: 'mint(address,string)',
+      }),
+    },
   });
 
   const app = express();
   app.use(cors());
   app.use(express.json());
 
-  app.get("/user", async (res) => {
+  app.get('/user', async (res) => {
     res.send(authenticatedUser);
   });
 
-  app.post("/message", async (req, res) => {
+  app.post('/message', async (req, res) => {
     const answer = await sdk.handleMessageFromFrontend(
       req.body.message,
       authenticatedUser.id,
