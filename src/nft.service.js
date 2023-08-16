980fe59668f8caacc611447b44378830e1b1ce49
Udi Hakim <udi@paytweed.com>
2023-08-16 13:40:01 +0300

    Added explentaion to the getById function

--- src/nft.service.js
+++ src/nft.service.js
@@ -9,10 +9,12 @@ class NftService {
       }
 
       /**
-       * 
-       * @param {string} id 
-       * @returns full metadata of the nft
-       */
+       * an example to an NFT checkout with the following mint function:
+       * safemint(toAddress address, tokenId uint256) payable public
+       * The abi should be taken from the compiled NFT contract
+       * The params should be populated in the customMintParams object
+       * There are two parameters that are getting populated from the frontend: toAddress and tokenURI
+       **/
     getById(id) {
         return {
             nftId: id,
