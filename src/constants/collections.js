d2cfdbe5aaf38630a14b6902ef4406159fa34e62
Alisa Makarova <alisa@paytweed.com>
2023-11-23 14:51:19 +0200

    add new chain collections

--- src/constants/collections.js
+++ src/constants/collections.js
@@ -3,10 +3,6 @@ const collectionsData = {
     contractAddress: "0xfae681f31e7a89c195fa3b1d16af351b9facc5b8",
     abi: "safeMint(toAddress address, tokenUri string)",
   },
-  ethereumGoerli: {
-    contractAddress: "0xc0dcf71fdd81457c439b3f2669ab6945f2074e3e",
-    abi: "safeMint(toAddress address, tokenUri string)",
-  },
   polygonMumbai: {
     contractAddress: "0xebe81fd78bd43873bcd30cb7312b75468686cc4f",
     abi: "safeMint(toAddress address, tokenUri string)",
@@ -28,20 +24,24 @@ const collectionsData = {
     abi: "safeMint(toAddress address, tokenUri string)",
   },
   tezosGhost: {
-    contractAddress: "KT1HPWEgkKawxjnq8L6569THCTG3DGD8XziL",
+    contractAddress: "KT1JZrySwb4p62fG88sVLwboYAvtGweo5SEY",
+    abi: "{}",
+  },
+  tezos: {
+    contractAddress: "KT1F6CPyEN4giWgNVdiVURmfRha6F7vqiaQH",
     abi: "{}",
   },
   polygon: {
     contractAddress: "0x90FCAb7b307128CEB4a678BDb5240F4bcF950875",
     abi: "mint(address,string)",
   },
-  arbitrumGoerli: {
-    contractAddress: "0x1bcb8607090dab9b584cf86bf55880cb0a0cedc3",
-    abi: "safeMint(toAddress address, tokenUri string)",
-  },
   arbitrum: {
     contractAddress: "0x4751Cd56231D4A92B607E779FD47802D3f771CAC",
     abi: "mint(address,string)",
+  },
+  arbitrumSepolia: {
+    contractAddress: "0x6C3E3f4090755629e04F13d69d8A600660DeB854",
+    abi: "safeMint(toAddress address, tokenUri string)",
   },
   fuseSpark: {
     contractAddress: "0x409975a38AEc7f7d5D43d1d46654d3215c8A2D0F",
