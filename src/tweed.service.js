598caecf2f0933bfaff3efd5d130babd51a9abfc
Alisa Makarova <alisa@paytweed.com>
2023-12-19 12:17:42 +0200

    add token service and upgrade sdk version

--- src/tweed.service.js
+++ src/tweed.service.js
@@ -1,14 +1,18 @@
-const { TweedBackendSDK } = require("@paytweed/backend-sdk");
+const { TweedBackendSDK, Environment } = require("@paytweed/backend-sdk");
 const nftService = require("./nft.service");
+const tokenService = require("./token.service");
 
 class TweedService {
   async initialize() {
     this._client = await TweedBackendSDK.setup({
-      apiKey: "YOUR-API-KEY",
-      apiSecret: "YOUR-API-SECRET",
+      apiKey: "rlSyUu57IElwqHXJjeSR1UXQRdpmFMRz",
+      apiSecret:
+        "gqyuZu2zzFcaITtU5i--l7rQwtGcbtXKJpbI9GwTJryct4SLqebSNbaZvoN0RBhB",
       defaultBlockchainIds: ["ethereumSepolia"],
       callbacks: {
         getNftPurchaseData: async ({ nftId }) => nftService.getById(nftId),
+        getTokenPurchaseData: async ({ tokenId }) =>
+          tokenService.getById(tokenId),
       },
     });
     return this._client;
