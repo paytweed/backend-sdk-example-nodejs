2657865dddfcd5430e036882b734f6e0859c36ef
Udi Hakim <udi@paytweed.com>
2023-12-22 22:03:48 +0200

    Secrets

--- src/tweed.service.js
+++ src/tweed.service.js
@@ -4,9 +4,9 @@ const nftService = require("./nft.service");
 class TweedService {
   async initialize() {
     this._client = await TweedBackendSDK.setup({
-      apiKey: "IcZ9nUMJKjolIpgKbc6OkkZdwHHJLJ6B",
-      apiSecret: "Le_rjSWd3QR4PauOw4nbEkOZ_ZnnbBUl0fUjLcsC5oV7i4-COJhJ6dXL8Nud7_60",
-      defaultBlockchainIds: ["bnbTestnet"],
+      apiKey: "<YOUR_API_KEY>",
+      apiSecret: "<YOUR_API_SECRET>",
+      defaultBlockchainIds: ["ethereumSepolia"],
       callbacks: {
         getNftPurchaseData: async ({nftId}) => nftService.getNFTById(nftId),
         getTokenPurchaseData : async({tokenId}) => nftService.getTokenById(tokenId)
